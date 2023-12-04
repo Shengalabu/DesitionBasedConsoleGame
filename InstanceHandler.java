6edcead68a48d0ca6bcd6d7a7712bd3361032196
Shengalabu <88543387+Shengalabu@users.noreply.github.com>
2023-12-04 13:49:08 +0800

    Added Main files

---
+++ InstanceHandler.java
@@ -0,0 +1,9 @@
+public class InstanceHandler{
+    
+    
+    public static void Run(){
+            System.out.println("Hello");
+    }
+    
+
+}
---
+++ Main.java
@@ -0,0 +1,9 @@
+import InstanceHandler;
+
+
+public class Main{
+    public static void main(String[] args) {
+        InstanceHandler hello = new InstanceHandler();
+        hello.Run();
+    }
+}
